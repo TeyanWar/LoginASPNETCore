b60e6aad7ecf3d6fbc978f7f73bf479e227ae466
Teyan <esteban.9411@hotmail>
2019-01-24 07:46:25 -0500

    n

---
+++ login_aspnet_core/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using login_aspnet_core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace login_aspnet_core
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+    }
+
+}
---
+++ login_aspnet_core/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace login_aspnet_core.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Full Name field is required"), StringLength(50, MinimumLength = 2)]
+        public string FullName { get; set; }
+
+        [Required, StringLength(20)]
+        public string Username { get; set; }
+
+        [Required, StringLength(50)]
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
